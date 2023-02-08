88b5279e188bc160ebd010128864f3ba613855d2
Sam Bencivengo <sambencivengo@gmail.com>
2023-02-07 21:35:43 -0700

    ingredients reference table... now to build some seed data....

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :sandwich_ingredients
+  resources :ingredients
   resources :orders
   resources :sandwiches
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_07_190334) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_08_043112) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "orders", force: :cascade do |t|
     t.bigint "sandwich_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["sandwich_id"], name: "index_orders_on_sandwich_id"
+  end
+
+  create_table "sandwich_ingredients", force: :cascade do |t|
+    t.bigint "sandwich_id", null: false
+    t.bigint "ingredient_id", null: false
+    t.index ["ingredient_id"], name: "index_sandwich_ingredients_on_ingredient_id"
+    t.index ["sandwich_id"], name: "index_sandwich_ingredients_on_sandwich_id"
   end
 
   create_table "sandwiches", force: :cascade do |t|
@@ -30,4 +44,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_190334) do
   end
 
   add_foreign_key "orders", "sandwiches"
+  add_foreign_key "sandwich_ingredients", "ingredients"
+  add_foreign_key "sandwich_ingredients", "sandwiches"
 end
